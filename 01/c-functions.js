c9a36a734a8b9eda5c8e627fa99fdc40c1a4f877
LDSpecIT <souza.lucianop@gmail.com>
2026-07-27 15:08:42 -0400

    afternoon 1

--- 01/c-functions.js
+++ 01/c-functions.js
@@ -28,6 +28,38 @@
 
 // ARROW FUNCTIONS:
 
+// const myArrowFunction = () => {
+//         return "I am an arrow FN";
+// }
+
+// ANONYMOUS FN //
+
+        // no name function " () => "
 
 
 
+        // ONE
+
+        
+function namedFn(firstName, lastName) {
+        
+        return `${firstName} ${lastName}`
+}
+
+
+        //TWO
+
+
+const getRemainder = (num, divisor) => {
+
+       return num % divisor
+}
+
+        // THREE
+
+function hours(open, close) {
+        console.log("We are open from" + " " + open + " to " + close) 
+}
+
+
+//RETURN 
