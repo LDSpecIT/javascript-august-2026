e0c43ed004a4875fabfedd3998ad38c72a5d51ad
LDSpecIT <souza.lucianop@gmail.com>
2026-07-27 11:28:52 -0400

    before br

--- 01/a-strings.js
+++ 01/a-strings.js
@@ -1,1 +1,54 @@
-// STRINGS //
+
+// variables
+// var, const, let --> var is older model that may not fit all framework code. Const is a constant that may not be changed in many times of its use. let is a variable condition call to a variable that can be changed.
+
+var numberOfHotDogsEaten = 15
+// This can be changed in console
+
+let numberOfPizesWon = 10
+// This can be changed by console
+
+const nameOfThemePark = "Coney Island"
+// This can not change on console
+
+
+// STRINGS
+
+// text
+
+const firstString = `A ton of text`
+const useOfQuotes = `I understand how to use \"quotes\" inside quotes`
+
+// interpolation --> let inside const changing as line changes
+let lineLength = 6
+const withBacktics = `Current Line Queue: ${lineLength}`
+
+// CONCATENATION this is one way to bring strings together
+
+const thingOne = "one"
+const thingTwo = "two";
+const thingThree = thingOne + thingTwo;
+
+
+// create two values
+
+
+// Using opening openTime and closeTime - create a prompt to let visitors known operating hours: Make sure that if you change the times constants, the strings will follow the change.
+
+const openTime = "7am";
+const closeTime = "12am";
+
+const operatingHours = `Our hours of Operation are ${openTime} through ${closeTime}`
+
+
+
+// Use Concatenation or Interpolation to create your full name.
+
+
+const firstName = "Luciano"
+const lastName = "DeSouza"
+
+const userFullname = firstName + " " +  lastName;
+
+
+
--- 01/b-numbers.js
+++ 01/b-numbers.js
@@ -1,1 +1,17 @@
 // NUMBERS //
+
+
+// Numbers Array
+// index:     0  1  2  3  4
+// const nums = [1, 2, 3, 4, 5]
+
+// Operators
+// const operator = [+, -, >=, <=, /, *, %, >, <, ==]
+
+
+
+
+
+// nums.push[6]
+// nums.pop[0]
+
