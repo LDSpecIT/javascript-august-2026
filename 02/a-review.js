d48ff3cf14c5f912abf3eff3cd3c1388ecd8dd96
LDSpecIT <souza.lucianop@gmail.com>
2026-07-28 11:01:30 -0400

    72826morning

--- 02/a-review.js
+++ 02/a-review.js
@@ -6,11 +6,34 @@
 // - otherwise returns `"ODD"`
 // - HINT: Use the modulo (%) operator
 
+function isDivisible(num) {
+
+    if ((num % 2) === 0) { 
+        return "EVEN"
+    } else {
+        return "ODD"
+    }
+
+}
+
+
 // Build a function `extremeWeather()`
 // - accepts argument `tempF` and `precipitation`
 // - if `tempF` is above 100 degrees or below 32 degrees return `true`
 // - if `precipitation` is above 12 return `true`
 // - otherwise return `false`
+
+function extremeWeather(tempF, precipitation){
+    if (tempF > 100 || tempF < 32){
+        return "TRUE"
+    } else if (precipitation > 12){
+        return "TRUE"
+    }  else {
+        return "FALSE"
+    }
+}
+
+
 
 // Build a function `fizzBuzz()`
 // - accepts an argument `num`
@@ -18,3 +41,24 @@
 // - if `num` is divisible by 5 return `"BUZZ"`
 // - if `num` is divisible by 3 and 5 return `"FIZZBUZZ"`
 // - otherwise return the number
+
+function fizzBuzz (num) {
+    if ((num % 3) === 0 && (num % 5) === 0) {
+    return "FIZZBUZZ" }
+    
+    else if ((num % 3) === 0){
+        return "FIZZ"
+    } 
+    
+    else if ((num % 5) === 0) {
+        return "BUZZ"
+    } 
+    
+    else {
+        return "Neither"
+    }
+}
+
+
+
+// const num = fizzBuzz() => if ((num % 3) === 0) {return "FIZZ"} 
