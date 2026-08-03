37dc9651db37c038fd238b67fbaf8b289f479690
LDSpecIT <souza.lucianop@gmail.com>
2026-08-03 12:59:05 -0400

    8.3.26 morning

--- 05/random-math.js
+++ 05/random-math.js
@@ -1,1 +1,73 @@
 // RANDOM MATH //
+
+
+
+// const arrayOfNumbers = [1,2,3,4,5]
+
+// arrayOfNumbers[0]
+// arrayOfNumbers[4]
+
+
+
+
+
+// let randomIndexOfStates = Math.floor (Math.random() * arrayOfStates.length)
+
+// let randomState = arrayOfStates[randomIndexOfStates]
+// console.log (randomState)
+
+
+function getRandomState() {
+    const arrayOfStates = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+
+    const randomNumber = Math.floor (Math.random() * arrayOfStates.length)
+
+    const randomState = arrayOfStates[randomNumber]
+
+    return randomState
+
+}
+
+
+
+function planAllAmericanVacation ( numberOfLocations ) {
+
+    const vacationArray = []
+
+    for (i = 0; i < numberOfLocations; i++) {
+        
+        const randomState = getRandomState()
+        
+        const formattedString = `${i + 1} ${randomState}`
+
+        vacationArray.push(formattedString)
+        
+        // console.log (formattedString)
+
+    }
+    
+    return vacationArray
+
+}
+
+
+
+
+
+const foods = [
+    "Pizza",
+    "Hamburger",
+    "Mac n Cheese",
+    "Falafel",
+    "Patato Salad"
+]
+function getRandomFood () {
+   
+    const foodChoice = Math.floor (Math.random () * foods.length)
+
+    return foods [foodChoice]
+
+}
+
+
+
--- 05/splitting-strings.js
+++ 05/splitting-strings.js
@@ -1,1 +1,45 @@
 // SPLITTING & JOINING STRINGS //
+
+
+    // .split() --> used to define a form of splitting a sentence, word, or string text. 
+        //Examples:
+            // banana.split("")
+            // theString.split("/[.?!]/") --> split by "., or ?, or !"
+            //theString.split(". ") --> split only by .
+    
+    // .join() --> Used to define a form of string joined together
+        //Examples:
+            //["firstString", "secondString"].join(" ")
+            //["firstString", "secondString"].join(". ")
+
+
+//CONVERT AN OBJECT TO AN ARRAY
+
+        //const string = Object.value ('object') | const string = Object.key ('object')
+
+
+//CAPITALIZING
+    // wordArray[0].toUpperCase
+    function capitalizeString ( str ) {
+        const splitString = str.split("")
+        splitString [0] = splitString[0].toUpperCase()
+        return splitString.join("")
+    
+    }
+
+
+
+// REVERSE A STRING & PATCHING
+
+
+
+
+
+//SCRAMBLE A WORD
+
+
+
+
+
+
+let theSentence = "The spry young fox jumped over the dog"
