d815f7d17bc9fee1815c1f063d2c3a46a85f9842
LDSpecIT <souza.lucianop@gmail.com>
2026-08-05 16:51:09 -0400

    endOfDay 8.5.26

--- 07/review.js
+++ 07/review.js
@@ -1,1 +1,23 @@
 // REVIEW //
+
+
+        // document.querySelector()
+        // document.querySelectorAll()
+        // document.getElementById()
+        // document.getElementsByClassName()
+
+        // const variable = document.querySelector('main')
+
+
+        // function manipulateSomething (parameter1) {
+        //     for (let i = 0; i < variable.length; i++){
+        //         variable[0].textContent.style.color = "red"
+        //     }
+
+        //     if (variable.textContent.includes("The")) {
+        //         variable[0].textContent.style.color = "blue"
+        //     }
+        // } 
+
+
+
