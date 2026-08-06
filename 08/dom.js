287df6a4098e99a21e87464e85e222c0130dbddf
LDSpecIT <souza.lucianop@gmail.com>
2026-08-06 12:52:52 -0400

    8.6.26 end of Morning

---
+++ 08/dom.js
@@ -0,0 +1,16 @@
+const Htwo = document.querySelectorAll('div')[1].querySelector('h2')
+// alternatively - const Htwo = document.querySelector(id)
+// alternatively - const HTwo = document.querySelectorAll('div')[1].children[0]
+
+
+document.querySelectorAll('div')[2].children[3].children[0].textContent
+
+
+document.querySelectorAll('div')[2].querySelector('div').children[3].querySelector('span')
+
+
+document.querySelector('textarea').value
+
+document.querySelector('#likes-about-js').value
+
+document.querySelectorAll('div')[1].querySelector('form').querySelector('textarea').value
