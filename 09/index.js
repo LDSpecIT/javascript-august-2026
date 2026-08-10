be3eeea09a1651946a1ce133fed6312df5ec8aa2
LDSpecIT <souza.lucianop@gmail.com>
2026-08-10 10:03:44 -0400

    8.10.26Morning

---
+++ 09/index.js
@@ -0,0 +1,16 @@
+
+
+const testOne = ["moose", "mouse", "house", "goose"]
+const testTwo = ["goose", "goose", "goose", "goose"]
+const testThree = []
+
+
+//rec: start at the end of the function and go to middle and then beginning
+
+function countGesse () {
+
+
+
+
+    return numberOfGeese
+}
