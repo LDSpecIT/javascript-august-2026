4ffac0c8accc1d778204bbeabbac8a098a37c166
brewchetta <chett.tiller@gmail.com>
2026-08-13 08:58:06 -0400

    add exercses to day 12

---
+++ 12/06-additional-practices.js
@@ -0,0 +1,22 @@
+// Reference the Exercises In 06-additional-practice.md for instructions
+
+
+// Exercise One - Triangles
+
+
+
+// Exercise Two - Farenheit to Celsius
+
+
+
+// Exercise Three - Cake
+
+
+
+// Exercise Four - FizzBuzz
+
+
+
+// Exercise Five - Geometry
+
+
---
+++ 12/07-dom-manipulation.js
@@ -0,0 +1,35 @@
+// DOM MANIPULATION EXERCISES
+
+
+// 1. Use `.querySelector()` to select the <form> and save it to the variable `topicForm`.
+
+
+// 2. Use `.querySelectorAll()` to select all <code> and save them to the variable `codeElements`.
+
+
+// 3. Use `.querySelector()` to select the <ul> and save it to the variable `topicsList`.
+
+
+// 4. Use `.querySelector()` to select the element with an id of "topic-input" and save it to the variable `topicInput`.
+
+
+// 5. Use `.querySelector()` to select the element with an id of "important-information" and save it to the variable `importantInformation`.
+
+
+// 6. Change the `topicForm` to use a "solid grey 2px" border styling.
+
+
+// 7. Change the styling for all <code> so that they are the color "red".
+
+
+// 8. Change the first item inside `codeElements` so its text properly read "defer".
+
+
+// 9. Remove the last <li> within `topicsList`.
+
+
+// 10. When the <button> is clicked, change the styling for every element within `topicList` to use a textDecoration of "line-through".
+
+
+// 11. When the <form> is submitted, add the current `value` from `topicInput` to the `topicsList` as a <li>.
+
